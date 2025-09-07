cff22657b7972c95a43c03fe91bfdbcc205cd795
Nazrulloh <rakhmatovnazrulloh@gmail.com>
2025-09-07 08:50:59 +0500

    added

--- game_bot_admin/settings.py
+++ game_bot_admin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0)+p%mma(t4v)!lidxv1-$^)p-apnt3c!f2a=u7p^h$qc7loy1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
