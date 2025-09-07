e48901cb24146719f03ad3a0cc1e62e10db4d698
Nazrulloh <rakhmatovnazrulloh@gmail.com>
2025-09-07 08:52:22 +0500

    added

--- game_bot_admin/settings.py
+++ game_bot_admin/settings.py
@@ -112,10 +112,14 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
+import os
+static_dir = os.path.join(BASE_DIR, "static")
+if os.path.exists(static_dir):
+    STATICFILES_DIRS = (static_dir,)
+else:
+    STATICFILES_DIRS = []
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
